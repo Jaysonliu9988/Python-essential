7ecfcd630c2bf3493712629357de3f4dba194a8f
Jaysonliu <leobest9688@gmail.com>
2021-09-10 17:26:38 +0800

    list in python

---
+++ list_in_python.py
@@ -0,0 +1,21 @@
+countries = ['China', 2, True, 'USA', 'Ghana']
+print(countries)
+print(countries[2])
+# print(countries[2][1])
+print(countries[1:])
+print(countries[1:3])
+print(countries[1:4])
+print(type(countries))
+countries[4] = 'Nigeria'
+print(countries)
+print(countries[-1])
+print(countries[-2])
+print(len(countries))
+print(type(countries[0]))
+print(type(countries[1]))
+print(type(countries[2]))
+fruits = list(('Apple', 23, False))
+print(fruits)
+
+
+
