62b7e582ab8b4e77219b84f36a3a0119585d6ac6
Jaysonliu <leobest9688@gmail.com>
2021-09-13 10:50:16 +0800

    dictionaries in python

---
+++ dictionaries_in_python.py
@@ -0,0 +1,21 @@
+my_dict = {
+    'name': 'John',
+    'name': 'Holy',
+    'name2': 'Mike',
+    'age': 34,
+    'is_tall': True,
+    'nationality': 'Australia',
+    'qualification': 'Bachelor',
+    'friends': ['Jake', 'Sunny', 'Mike']
+}
+
+print(my_dict)
+print(len(my_dict))
+print(my_dict['name'])
+print(my_dict['age'])
+print(my_dict['friends'])
+
+print(type(my_dict))
+
+x = my_dict['qualification']
+print(x)
