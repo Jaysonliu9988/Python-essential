d0f644f9ffd277758f05fd730796cc378b3191f3
Jaysonliu <leobest9688@gmail.com>
2021-09-13 10:28:12 +0800

    if statement in python

---
+++ if_statement_in_python.py
@@ -0,0 +1,20 @@
+a = 4
+b = 3
+
+if a>b:
+    print(f'{a} is greater than {b}')
+
+value = int(input('Enter a number: '))
+
+if value%5 == 0:
+    print(f'{value} can be divided by 5')
+else:
+    print(f'{value} can\'t be divided by 5')
+
+
+num = int(input('Enter a number: '))
+
+if num%2 == 0:
+    print('Even number')
+else:
+    print('Old number')
