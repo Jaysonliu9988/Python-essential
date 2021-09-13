18934a37437229ad4979c3e054cbcd5c4db04800
Jaysonliu <leobest9688@gmail.com>
2021-09-13 11:11:54 +0800

    for loops in python

---
+++ for_loops.py
@@ -0,0 +1,31 @@
+for letter in 'Hello':
+    print(letter)
+
+print()
+for ninin in 'Hello':
+    print(ninin)
+
+my_dict = {
+    'name': 'Mike',
+    'age': '23'
+}
+
+for values in my_dict:
+    print(values)
+
+
+my_list = ['ji', 'ju', 'jk']
+
+for values in my_list:
+    print(values)
+    if values == 'ju':
+        break
+
+print()
+my_list2 = ['ji', 'ju', 'jk']
+
+for values in my_list2:
+    # print(values)
+    if values == 'ju':
+        break
+    print(values)
