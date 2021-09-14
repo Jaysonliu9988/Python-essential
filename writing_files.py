c80bde62c3d4fd634f68ab84a078442c1e15a0d2
Jaysonliu <leobest9688@gmail.com>
2021-09-14 11:40:31 +0800

    writing files in python

---
+++ writing_files.py
@@ -0,0 +1,9 @@
+# coun_file = open('/Users/jayson/Linkedin Course/Python-essential/country.txt', 'w')
+# coun_file = open('/Users/jayson/Linkedin Course/Python-essential/country.txt', 'a')
+coun_file = open('/Users/jayson/Linkedin Course/Python-essential/new.py', 'w')
+
+# coun_file.write('This is the new file')
+# coun_file.write('\n This is the new line')
+coun_file.write("print('This is the new line')")
+
+
