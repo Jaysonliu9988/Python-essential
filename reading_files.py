c33a35446fbabcb41a5c0e20132141ce4e9362f2
Jaysonliu <leobest9688@gmail.com>
2021-09-14 11:31:56 +0800

    reading files in python

---
+++ reading_files.py
@@ -0,0 +1,14 @@
+coun_file = open('/Users/jayson/Linkedin Course/Python-essential/countries.txt', 'r')
+
+# print(coun_file.readline())
+# print(coun_file.readlines())
+# print(coun_file.readlines()[1])
+
+
+for files in coun_file.readlines():
+    print(files)
+
+
+
+
+coun_file.close()
