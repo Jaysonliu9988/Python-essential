a922e8184a4a5844e73ad4dedf46537dd6c95bc1
Jaysonliu <leobest9688@gmail.com>
2021-09-21 10:56:04 +0800

    display to the file

--- test_generator.py
+++ test_generator.py
@@ -1,13 +1,15 @@
 import random
 op = input('Addition or subtraction test? [enter "+" or "-"]: ')
+output = open('output.txt', 'w')
+
 
 if op == '+':
-    print('Addition Test\n')
+    output.write('Addition Test\n')
 else:
-    print('Subtraction Test\n')
+    output.write('Subtraction Test\n')
     op = '-'
 
-print('Name: __________\n\n')
+output.write('Name: __________\n\n')
 
 for i in range(10):
     num1 = random.randint(0, 9)
@@ -16,4 +18,7 @@ for i in range(10):
     if op == '-' and num1 < num2:
         num1, num2 = num2, num1
 
-    print(num1, op, num2, '= ____\n')
+    output.write(f'{num1} {op} {num2} = ____\n')
+
+output.close()
+print('Test saved in output.txt file.')
