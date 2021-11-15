cca3eb91c8405a481171a4db929b4494f387f2f9
jayson <leobest9688@gmail.com>
2021-11-15 16:07:15 +0800

    add the introduction

---
+++ calculate_tax.py
@@ -0,0 +1,34 @@
+# You have been asked to write a simple program involving calculating income tax, and have been provided with this table of information from the Australian Tax Office website:
+
+# A) Write either pseudocode or a flowchart (your choice) that illustrates the design of a function which receives an income as a parameter and returns the amount of tax, calculated using the information in the table above.
+
+# B) Write Python code for a function named “calculate_tax()” which implements your design. The function must:
+#    Accept an “income” parameter (assume that it is a float).
+#    Calculate the appropriate amount of tax.
+#    Return the amount of tax as a float, rounded to two decimal places.
+
+# C) Write a program that does the following (and uses your function as needed):
+#    Prompt the user to enter their old income and attempt to convert it to
+#       a float (re-prompt the user for input until they enter a float).
+#    Prompt the user to enter their new income and attempt to convert it to
+#       a float (re-prompt the user for input until they enter a float).
+#       Hint: Use exception handling and a loop to re-prompt until a float is entered.
+#    Calculate and store the amount of tax for the old and new incomes.
+#    If their new income is more than their old income, print messages saying how much more they now earn and how much more tax they must pay.
+#    Otherwise, if their new income is less than their old income, print messages saying how much less they now earn and how much less tax they must pay.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
