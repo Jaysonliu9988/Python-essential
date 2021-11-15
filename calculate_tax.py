0391a45ada273fa664e300ee254a04d8a54d9264
jayson <leobest9688@gmail.com>
2021-11-15 16:08:11 +0800

    add the calculate function and loop

--- calculate_tax.py
+++ calculate_tax.py
@@ -18,6 +18,43 @@
 #    Otherwise, if their new income is less than their old income, print messages saying how much less they now earn and how much less tax they must pay.
 
 
+def get_tax(income: float):
+    if 0 < income <= 18200:
+        tax = 0
+    elif 18200 < income <= 37000:
+        tax = (income - 18200) * 0.19
+    elif 37000 < income <= 90000:
+        tax = 3572 + (income - 37000) * 0.325
+    elif 90000 < income <= 180000:
+        tax = 20797 + (income - 90000) * 0.37
+    else:
+        tax = 54097 + (income - 180000) * 0.45
+    return tax
+
+while True:
+    try:
+        old_income = float(input('What is your old taxable income?: '))
+        break
+    except:
+        print('You should input a float num.')
+while True:
+    try:
+        new_income = float(input('What is your new taxable income?: '))
+        break
+    except:
+        print('You should input a float num.')
+
+old_income_tax = get_tax(income=old_income)
+new_income_tax = get_tax(income=new_income)
+dif_income = abs(new_income - old_income)
+dif_tax = abs(new_income_tax - old_income_tax)
+if new_income > old_income:
+    print('Your income has increase ${:.2f}'.format(dif_income))
+    print('The tax you have to pay is increase ${:.2f}'.format(dif_tax))
+
+else:
+    print('Your income has decreased ${:.2f}'.format(dif_income))
+    print('The tax you have to pay is reduced ${:.2f}'.format(dif_tax))
 
 
 
